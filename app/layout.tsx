ccfab4403999c0e8e38606f0a0cb0bf459a6cbeb
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-05-01 19:43:01 -0700

    chore: implement initial settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lexend_Deca } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Lexend_Deca({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col bg-gray-200 py-8 pl-16">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
